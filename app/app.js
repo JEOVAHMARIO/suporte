33c289088a61eef602cb404383fb28fbf40c5acf
JEOVAHMARIO <135380760+JEOVAHMARIO@users.noreply.github.com>
2023-10-20 14:10:51 -0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,105 @@
+const http = require('http');
+
+const PORT = 3000;
+const server = http.createServer(function (req, res) {
+    let [url, queryString] = req.url.split('?');
+
+    if (url === '/index') {
+        index(req, res);
+    } else if (url === '/calcular') {
+        calcularArea(req, res, queryString);
+    } else if (url === '/autor') {
+        autor(req, res);
+    } else {
+        naoEncontrado(req, res);
+    }
+});
+
+function index(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    const responseBody = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>Problema: Cálculo da Área de um Suporte Octogonal!</h1>
+    <p><strong>Problema:</strong></p>
+    <p>Determine a área de um suporte octogonal. Se a área for menor que 20 metros quadrados, é um suporte pequeno. Se estiver fora desse intervalo, é um suporte especial.</p>
+    <form action="/calcular" method="get">
+    <label>
+    <span>Lado (em metros)</span>
+    <input name="lado">
+    </label>
+    <button>Calcular</button>
+    </form>
+    </body>
+    </html>`;
+    res.end(responseBody);
+}
+
+function calcularArea(req, res, queryString) {
+    const params = new URLSearchParams(queryString);
+    const lado = parseFloat(params.get('lado'));
+
+    if (isNaN(lado)) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 400;
+        res.end('Lado inválido. Certifique-se de fornecer um número válido.');
+    } else {
+        const area = calcularAreaOctogonal(lado);
+
+        res.setHeader('Content-Type', 'text/plain');
+        const message = `Lado: ${lado} metros\nÁrea: ${area.toFixed(2)} metros quadrados\nExplicação do Cálculo: ${
+            area < 20 ? 'um suporte pequeno.' : 'um suporte especial.'
+        }\n\nDesenvolvido por Jeovah`;
+        res.end(message);
+    }
+}
+
+function calcularAreaOctogonal(lado) {
+    return 2 * (1 + Math.sqrt(2)) * Math.pow(lado, 2);
+}
+
+function naoEncontrado(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 404;
+    const responseBody = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>Não encontrado!</h1>
+    </body>
+    </html>`;
+    res.end(responseBody);
+}
+
+function autor(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    const responseBody = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>Autor</h1>
+    <p>Nome: Jeovah Mario </p>
+    <h2>Formações Acadêmicas</h2>
+    <ul><li>Técnico em Eletromecânica</li></ul>
+    <h2>Experiências Profissionais</h2>
+    <ul>
+    <li>Estagiário</li>
+    <li>Seção de motores</li>
+    <li>Ano: 2020</li>
+    </ul>
+    <footer>Desenvolvido por Jeovah Mario</footer>
+    </body>
+    </html>`;
+    res.end(responseBody);
+}
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
