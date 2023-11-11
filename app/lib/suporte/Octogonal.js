49eb520339b7c56af2f8ac572b708c7c44cc7399
JEOVAHMARIO <135380760+JEOVAHMARIO@users.noreply.github.com>
2023-11-11 15:47:27 -0300

    Update Octogonal.js

--- app/lib/suporte/Octogonal.js
+++ app/lib/suporte/Octogonal.js
@@ -1,19 +1,27 @@
 class Octogonal {
-    constructor(nome, lado) {
+    constructor(nome, lado, senha, papel) {
         this.nome = nome;
-        this.lado = lado;
-        this.area = this.calcularArea();
-        this.explicacao = this.area < 20 ? 'um suporte pequeno.' : 'um suporte especial.';
-        this.tipo = 'desconhecido';
+        this.lado = parseFloat(lado);
+        this.senha = senha;
+        this.papel = papel;
+        this.calcularArea();
     }
 
     calcularArea() {
-        return 2 * this.lado * this.lado * (1 + Math.sqrt(2));
+        if (!isNaN(this.lado)) {
+            this.area = 2 * this.lado * this.lado * (1 + Math.sqrt(2));
+            this.explicacao = this.area < 20 ? 'um suporte pequeno.' : 'um suporte especial.';
+            this.tipo = this.area < 20 ? 'pequeno' : 'especial';
+        } else {
+            this.area = 0;
+            this.explicacao = 'Comprimento do lado inválido.';
+            this.tipo = 'desconhecido';
+        }
     }
+
     setLado(lado) {
-        this.lado = lado;
-        this.area = this.calcularArea();
-        this.explicacao = this.area < 20 ? 'um suporte pequeno.' : 'um suporte especial.';
+        this.lado = parseFloat(lado);
+        this.calcularArea();
     }
 }
 
