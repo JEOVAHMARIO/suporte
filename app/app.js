0b5d85e786eb673b1293855b3251539d44dbd667
JEOVAHMARIO <135380760+JEOVAHMARIO@users.noreply.github.com>
2023-12-02 17:51:11 -0300

    add fetch

--- app/app.js
+++ app/app.js
@@ -5,21 +5,26 @@ const AutorController = require('./controllers/AutorController');
 const AuthController = require('./controllers/AuthController');
 const SuporteMysqlDao = require('./lib/suporte/SuporteMysqlDao');
 const SuporteDao = require('./lib/suporte/SuporteDao');
+const UsuariosController = require('./controllers/UsuariosController');
+const UsuariosDao = require('./lib/suporte/UsuariosDao');
+const UsuariosMysqlDao = require('./lib/suporte/UsuariosMysqlDao');
 const mysql = require('mysql');
 
-const pool  = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'bd',
-    user            : process.env.MARIADB_USER,
-    password        : process.env.MARIADB_PASSWORD,
-    database        : process.env.MARIADB_DATABASE
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'bd',
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PASSWORD,
+    database: process.env.MARIADB_DATABASE
 });
 
 let suporteDao = new SuporteMysqlDao(pool);
+let usuariosDao = new UsuariosMysqlDao(pool);
 let suporteController = new SuporteController(suporteDao);
 let estaticoController = new EstaticoController();
 let autorController = new AutorController();
-let authController = new AuthController(suporteDao);
+let authController = new AuthController(usuariosDao);
+let usuariosController = new UsuariosController(usuariosDao);
 
 const PORT = 3000;
 
@@ -37,20 +42,32 @@ const server = http.createServer(async (req, res) => {
         } else if (method === 'POST') {
             suporteController.inserir(req, res);
         } else if (method === 'PUT') {
-            authController.autorizar(req, res, function() {
+            authController.autorizar(req, res, function () {
                 suporteController.alterar(req, res);
-            }, ['admin', 'geral']); 
+            }, ['admin', 'geral']);
         } else if (method === 'DELETE') {
-            authController.autorizar(req, res, function() {
+            authController.autorizar(req, res, function () {
                 suporteController.apagar(req, res);
-            }, ['admin']); 
+            }, ['admin']);
         } else {
             estaticoController.naoEncontrado(req, res);
         }
+    } else if (endpoint === 'usuarios' && method === 'GET') {
+        usuariosController.listar(req, res);
+    } else if (endpoint === 'usuarios' && method === 'POST') {
+        usuariosController.inserir(req, res);
+    } else if (endpoint === 'usuarios' && method === 'PUT') {
+        authController.autorizar(req, res, function () {
+            usuariosController.alterar(req, res);
+        }, ['admin', 'geral']);
+    } else if (endpoint === 'usuarios' && method === 'DELETE') {
+        authController.autorizar(req, res, function () {
+            usuariosController.apagar(req, res);
+        }, ['admin']);
     } else if (endpoint === 'login') {
-        authController.index(req, res); 
+        authController.index(req, res);
     } else if (endpoint === 'logar') {
-        authController.logar(req, res); 
+        authController.logar(req, res);
     } else if (endpoint === 'autor') {
         autorController.autor(req, res);
     } else {
